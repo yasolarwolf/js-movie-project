a9665d813d09eee6c1066d5765569bc2f0e65081
Nina <aseeva.nina@yahoo.com>
2022-05-11 14:10:01 +0200

    add cycles

--- script.js
+++ script.js
@@ -8,12 +8,30 @@ const personalMovieDB = {
     privat: false
 }
 
-let a = prompt('Один из последних просмотренных фильмов', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов', ''),
-    d = prompt('На сколько оцените этот фильм?', '');
+for (let i = 0; i < 2; i++) {
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    const a = prompt('Один из последних просмотренных фильмов', '');
+    b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a !== ' ' && b !== ' ' && a.length < 50) {
+        personalMovieDB.movies.a = b;
+    } else {
+        i--;
+    }
+}
+
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы настоящий киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
+
+
+
